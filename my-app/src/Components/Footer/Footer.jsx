b9c9b539a2e983046ec1b6f7558ab94940c81c3d
Olusegun Joseph <segunenikan@gmail.com>
2025-10-22 17:16:01 +0100

    new assets

---
+++ my-app/src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p><strong>Responsive Tools</strong> is a simple set of tools for building responsiv	sites and apps. All of my templates at <a href="http://html5up.net">HTML5 UP</a> are built using these tools.</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>234-798-777-3333</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 @ Food-depot.com All Right Reserved</p>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ my-app/src/context/storeContext.jsx
@@ -0,0 +1,46 @@
+import { createContext } from "react";
+import { food_list } from "../assets/assets";
+import { useEffect } from "react";
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+    const [cartItem,setCartItems] = useState({});
+
+    const  addToCart = (itemId) => {
+        if (!cartItem[itemId]) {
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems)
+    }
+
+    const contextValue ={
+        food_list,
+        cartItem,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+
+}
+
+
+export default StoreContextProvider;
